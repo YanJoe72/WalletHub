ec1dc4679b7e11d51fe1e2331018766f0caae64b
Gab <gabri.barreda@gmail.com>
2025-06-05 15:16:20 +0200

    ya quand même le warning hein

--- screens/HomeScreen.tsx
+++ screens/HomeScreen.tsx
@@ -4,7 +4,7 @@ import { ThemedText } from '@/components/ThemedText';
 import { ThemedView } from '@/components/ThemedView';
 import ParallaxScrollView from '@/components/ParallaxScrollView';
 import userData from '@/constants/user';
-import Compte  from '@/components/ui/Acceuil/Compte';
+import BankCard  from '@/components/ui/Acceuil/BankCard';
 import Depense  from '@/components/ui/Acceuil/Depenses';
 import Button  from '@/components/ui/Connexion/KeyButton';
 
@@ -22,77 +22,89 @@ const handleScroll = (event: NativeSyntheticEvent<NativeScrollEvent>) => {
   setActiveIndex(index);
 };
 
-//---------COMPTE-----------
+//---------BankCard-----------
 
 const comptes = [
-  { nom: 'N26', solde: '1230.45' },
-  { nom: 'Revolut', solde: '980.00' },
-  { nom: 'Société Générale', solde: '540.75' },
-  { nom: 'Lydia', solde: '215.10' },
-  { nom: 'Caisse d’Épargne', solde: '880.00' },
+  { title: 'N26', amount: '1230.45',cardNumber:'0001 0002 300', backgroundColor:'#F9E79F'},
+  { title: 'Revolut', amount: '980.00',cardNumber:'154845 84845 4558', backgroundColor:'#FFD6B0'},
+  { title: 'Société Générale', amount: '540.75' ,cardNumber:'1784848 9598485 8958 ',backgroundColor: '#B8E0D2'},
+  { title: 'Lydia', amount: '215.10', cardNumber:'741 852 963',backgroundColor: '#B5B9FF'},
+  { title: 'Caisse d’Épargne', amount: '880.00' , cardNumber:'12345 789 7862',backgroundColor:'#A7C7E7'},
 ];
 
 //---------DEPENSES-----------
  const depenses = [
     {
       id: 1,
-      imageSource: require('@/assets/images/netflixlogo.png'),
+      imageSource: require('../assets/images/netflixlogo.png'),
       title: "Abonnement mensuel Netflix ",
       montant: "29.99 €",
     },
     {
       id: 2,
-      imageSource: require('@/assets/images/crabGamelogo.png'),
+      imageSource: require('../assets/images/crabGamelogo.png'),
       title: "Crab game : Jeux",
       montant: "50.99€",
     },
     {
       id: 3,
-      imageSource: require('../../assets/images/photologo.png'),
+      imageSource: require('../assets/images/photologo.png'),
       title: "Appareil Photo",
       montant: "64.99€",
+    },
+    {
+      id: 4,
+      imageSource: require('../assets/images/favicon.png'),
+      title: "Abonnement mensuel ExpoGo ",
+      montant: "100.99€",
+    },
+    {
+      id: 5,
+      imageSource: require('../assets/images/netflixlogo.png'),
+      title: "Encore Netflix mais Prenium",
+      montant: "59.99 €",
     },
   ];
 
 //---------VUES-----------
   return (
     <SafeAreaView style={styles.safeArea}>
+      <ScrollView
+        showsVerticalScrollIndicator={false}
+        contentContainerStyle={styles.scrollContainerVertical}
 
-    <ThemedView backgroundColor='white'>
+      >
+<View style={styles.header}>
+
       {/* Conteneur avec flexDirection row pour les éléments côte à côte */}
-      <View style={{ flexDirection: 'row', justifyContent: 'space-between', alignItems: 'center' ,marginTop:25}}>
+      <View style={{marginTop:50,marginBottom:-150, marginLeft:150, flexDirection: 'row', justifyContent: 'space-between', alignItems: 'center' }}>
         {/* KeyButton 1 avec image */}
-        <Button
 
-          digit="1"
-          onPress={(digit) => console.log(digit)}
-          imageSource={require('@/assets/images/Setting.png')} // Remplace par ton image
-        />
 
-        <ThemedText style={{color:'black'}} type="title">Home</ThemedText>
+        <ThemedText style={{color:'#A9A9A9' }} type="title">Home</ThemedText>
 
         {/* KeyButton 2 avec image */}
         <Button
           digit="2"
           onPress={(digit) => console.log(digit)}
-          imageSource={require('@/assets/images/Notification.png')} // Remplace par ton image
+          imageSource={require('../assets/images/Notification.png')} // Remplace par ton image
         />
       </View>
-    </ThemedView>
 
-      {/* ScrollView avec les comptes */}
+
+      {/* ScrollView avec les BankCards */}
       <ScrollView
         horizontal
          pagingEnabled
+
          onScroll={handleScroll}
          scrollEventThrottle={16}
          showsHorizontalScrollIndicator={false}
          contentContainerStyle={styles.scrollContent}
          style={styles.scrollContainerHorizontal}>
-        {comptes.map((compte, i) => (
-//           <Compte key={i} nomBanque={compte.nom} solde={compte.solde} />
-            <View key={i} style={{marginTop:50 ,height:200,width:'400', alignItems: 'center' }}>
-                <Compte nomBanque={compte.nom} solde={compte.solde} />
+        {comptes.map((bank, i) => (
+            <View key={i} style={{height:100,width:width, alignItems: 'center' }}>
+                <BankCard  key={i}  title={bank.title} amount={bank.amount} cardNumber={bank.cardNumber} backgroundColor={bank.backgroundColor}/>
               </View>
         ))}
       </ScrollView>
@@ -111,34 +123,36 @@ const comptes = [
       <View style={{ flexDirection: 'row', justifyContent: 'center', alignItems: 'center' ,marginTop:25}}>
               {/* KeyButton 1 avec image */}
               <Button
+                style={styles.button}
                 digit="1"
                 onPress={(digit) => console.log(digit)}
-                imageSource={require('@/assets/images/Virement.png')} // Remplace par ton image
+                imageSource={require('../assets/images/Virement.png')} // Remplace par ton image
               />
               <Button
+              style={styles.button}
                 digit="1"
                 onPress={(digit) => console.log(digit)}
-                imageSource={require('@/assets/images/Scan.png')} // Remplace par ton image
+                imageSource={require('../assets/images/Scan.png')} // Remplace par ton image
               />
               <Button
+                style={styles.button}
                 digit="1"
                 onPress={(digit) => console.log(digit)}
-                imageSource={require('@/assets/images/Document.png')} // Remplace par ton image
+                imageSource={require('../assets/images/Document.png')} // Remplace par ton image
               />
 
       </View>
+   </View>
 
 <ThemedText style={{marginLeft:30,color:'gray',  fontWeight: 'bold' }}>Activité récentes:</ThemedText>
-            <ScrollView
-              vertical
-              showsHorizontalScrollIndicator={false}
-              contentContainerStyle={styles.scrollContent}
-              style={styles.scrollContainerVertical}
-            >
+
+
+<View style={styles.scrollContent}>
               {depenses.map((depense, i) => (
                 <Depense key={depense.id} imageSource={depense.imageSource} title={depense.title} montant={depense.montant} />
               ))}
-            </ScrollView>
+</View>
+                 </ScrollView>
     </SafeAreaView>
 
   );
@@ -156,14 +170,17 @@ const styles = StyleSheet.create({
   },
   stepContainer: {
     gap: 8,
-    marginBottom: 8,
-  },
+
+  },header: {
+      backgroundColor: 'white',
+      height:480,
+    },
 
   scrollContainerVertical:{
-        maxHeight: 500,
+          paddingBottom: 20
       },
   scrollContainerHorizontalHorizontal: {
-        maxHeight: 100, // hauteur max (tes cards font ~100px de haut)
+
       },
   scrollContent: {
     paddingHorizontal: 1,
@@ -173,7 +190,7 @@ const styles = StyleSheet.create({
     flexDirection: 'row',
     justifyContent: 'center',
     alignItems: 'center',
-    marginTop: 10,
+
     gap: 8,
   },
   dot: {
@@ -189,3 +206,9 @@ const styles = StyleSheet.create({
 
 
 
+
+// import HomeScreen from '@/Screen/HomeScreen';
+//
+// export default function Home(){
+//     return <HomeScreen/>;
+// }
